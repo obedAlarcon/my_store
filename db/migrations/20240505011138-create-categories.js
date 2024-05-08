b15592b3a4c36cc293f16ab652af8c8667836136
obedAlarcon <movi3526@hotmail.com>
2024-05-07 19:25:44 -0500

    primer

---
+++ db/migrations/20240505011138-create-categories.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { CATEGORY_TABLE, CategorySchema } = require('../models/category.model');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+  await queryInterface.createTable(CATEGORY_TABLE,CategorySchema);
+  
+  
+  
+  
+  
+  },
+
+  async down (queryInterface, Sequelize) {
+  await queryInterface.dropTable(CATEGORY_TABLE)
+  
+  
+  
+  
+  
+  }
+};
